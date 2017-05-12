b91d47c2d970c9b383701327d7406f8b83ffaa82
Eric Brearley <eric.r.brearley@gmail.com>
2017-05-12 18:29:03 +1000

    Initialise material UI

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  entry: {
+    main: [
+      'webpack/hot/only-dev-server',
+      './src/app/app.js',
+    ],
+  },
+  devServer: {
+    contentBase: 'src/www',
+    hot: true,
+    inline: true,
+    port: 3000,
+    host: 'localhost',
+  },
+  devtool: 'eval',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new TransferWebpackPlugin([{ from: 'www' }], path.resolve(__dirname, 'src')),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
